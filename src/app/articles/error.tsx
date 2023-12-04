11e79c3c137b14e4dac134f122bf480ff430ab2d
amirhrb <amirrabi70@gmail.com>
2023-12-04 13:05:13 +0330

    error boundary added to articles route

---
+++ src/app/articles/error.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+// react
+import { useEffect } from "react";
+// styles
+import styles from "./error.module.css";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className={styles.container}>
+      <h2>Something went wrong!</h2>
+      <button
+        type="button"
+        className="btn btn-dark"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
