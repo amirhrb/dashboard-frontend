adbd07b1b43baac604d2be3db80a31f5d6e1338e
Amirhosein Rabibeigi <81120130+amirhrb@users.noreply.github.com>
2023-12-06 01:13:09 +0330

    Update paginationFn.ts and better comment

--- src/utils/paginationFn.ts
+++ src/utils/paginationFn.ts
@@ -14,14 +14,33 @@ export const paginationHandler = ({
   const left = page > 1 ? page - 1 : page;
   const right = page < allPagesCount ? page + 1 : allPagesCount;
   let middleButtonsNumbers;
-  if (page === 1)
+  /* Note: this clause makes [0, 1, 2, 3] array 
+  in map methods callback we do the mathmatical operation to get proper array
+    Array.from(Array(buttonsCount).keys()).map(
+      (_i, index) => index 
+    );
+  */
+  if (page <= 1){
+    // in page 1 below (maybe negative number come from url) it should be [1, 2, 3, 4]
     middleButtonsNumbers = Array.from(Array(buttonsCount).keys()).map(
       (_i, index) => index + 1
     );
-  else
+  }
+    // minus 2 down below is because last two page should not show not existing pages
+  else if(page > 1 && page <= allPagesCount - 2){
+    // if page is (1, ensingPage-2] it should be [num-1, num, num+1, num+2]
     middleButtonsNumbers = Array.from(Array(buttonsCount).keys()).map(
       (_i, index) => index + page - 1
     );
+  } 
+  else{
+    /* in last two page and 
+    page number over all amount just show 
+    [last - 3, last - 2, last - 1, last] */
+    middleButtonsNumbers = Array.from(Array(buttonsCount).keys()).map(
+      (_i, index) => index + allPagesCount - 3
+    );
+  }
   return {
     left,
     right,
